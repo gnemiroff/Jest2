931d8c4e53ef1a38a9a0d3cb51a8d3f8e2c594d8
gnemiroff <53767867+gnemiroff@users.noreply.github.com>
2022-04-07 15:28:27 +0200

    Update functions.js
    Nemirovski mit doppeltem i zu Testzwecken

--- functions.js
+++ functions.js
@@ -13,7 +13,7 @@ const functions = {
     createUser: ()=> {
         const user = {
             firstName: "German",
-            secondName: "Nemirovski"
+            secondName: "Nemirovskii"
         }
         return user;
     },
