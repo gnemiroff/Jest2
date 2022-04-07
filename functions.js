54e02cfd971adce64d28730ff2b3a9655699b91a
gnemiroff <53767867+gnemiroff@users.noreply.github.com>
2022-04-07 15:29:23 +0200

    Nemirovski mit drei iii zu Testzweclen

--- functions.js
+++ functions.js
@@ -13,7 +13,7 @@ const functions = {
     createUser: ()=> {
         const user = {
             firstName: "German",
-            secondName: "Nemirovskii"
+            secondName: "Nemirovskiii"
         }
         return user;
     },
