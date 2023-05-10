19b7f8bd18641c2f64623baf4202fb346ee681af
gnemiroff <53767867+gnemiroff@users.noreply.github.com>
2023-05-11 00:01:52 +0200

    Update user.test.js
    Changed the text of a test.

--- user.test.js
+++ user.test.js
@@ -1,6 +1,6 @@
 const user = require('./user'); 
 
-test('should output name and age given a generated Object', () => {
+test('should output name and age given a generated Object !', () => {
     let user22 = user.Builder('88C').withName('Kevin').withAge(30).build();
     expect(user22._age).toBe(30);
 });
